12e1f73670b9917747a2af182ba91872d98d5737
iliash.work@mserverone.com <iliash.work@mserverone.com>
2018-10-10 16:47:02 +0300

    H1

--- src/components/comment-list.js
+++ src/components/comment-list.js
@@ -3,22 +3,27 @@ import Comment from './comment';
 import toggleDecorator from '../decorators/toggle'
 
 class CommentList extends Component {
+  state = {
+    isOpen: false
+  };
 
   render() {
+     const text = this.state.isOpen ? 'Close comments' : 'Open comments';
+     const commentBody = this.state.isOpen ? <ul>{this.commentItem}</ul> : '';
+
      return (
        <div>
-         <button onClick={this.commentToggle}>Open Comments</button>
+           <h3>Comments: <button onClick={this.commentToggle}>{text}</button></h3>
 
-         <h3>Comments:</h3>
-         <ul>
-           {this.commentItem}
-         </ul>
+           {commentBody}
        </div>
      )
   }
 
-  commentToggle() {
-    console.log('---click!');
+  commentToggle = () => {
+      this.setState({
+          isOpen: !this.state.isOpen
+      });
   }
 
   get commentItem() {
@@ -29,7 +34,7 @@ class CommentList extends Component {
 
         <Comment
           comment={comment.text}
-          // isOpen={openItemId === comment.id}
+          //isOpen={openItemId === comment.id}
           // toggleOpen={toggleOpenItem}
         />
 
